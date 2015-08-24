da787c9a8737ee960501062eaecc7b5a3c3889d7
sophiag <sophiag@INsophiag.local>
2015-08-24 18:18:35 +0530

    third commit

--- src/com/company/Item.java
+++ src/com/company/Item.java
@@ -6,9 +6,10 @@ package com.company;
 public class Item {
     private String itemName;
     private int price;
+    private int shelfPrice;
     private boolean isItImported;
     private boolean isSalesTaxApplicable;
-    private int salesTax, importTax;
+    private int salesTax, importTax, totalTax;
 
     public Item(String itemName, int price, boolean isItImported){
         this.itemName = itemName;
@@ -19,13 +20,34 @@ public class Item {
         setTaxValues();
     }
 
-    private setTaxValues(){
+    private void setTaxValues(){
         if(itemName=="medicine"||itemName=="bood"||itemName=="food"){
             salesTax = 0;
             isSalesTaxApplicable = false;
         }
 
-        int taxRate =
+        int taxRate;
+        if(isItImported&&isSalesTaxApplicable)
+            taxRate = 15;
+        else if(isItImported)
+            taxRate = 5;
+        else if(isSalesTaxApplicable)
+            taxRate = 10;
+        else
+            taxRate = 0;
+
+        shelfPrice = (price/((taxRate/100)+1));
+
+        totalTax = price - shelfPrice;
+
+        if(isItImported&&isSalesTaxApplicable){
+            salesTax = totalTax*(2/3);
+            importTax = totalTax*(1/3);
+        }
+        else if(isItImported)
+            importTax = totalTax;
+        else if(isSalesTaxApplicable)
+            salesTax = totalTax;
     }
 
 
--- src/com/company/ShoppingBasket.java
+++ src/com/company/ShoppingBasket.java
@@ -5,5 +5,14 @@ package com.company;
  */
 public class ShoppingBasket {
     int numberOfItems;
+    Item[] itemList;
+
+    public ShoppingBasket(int numberOfItems){
+        this.numberOfItems = numberOfItems;
+
+        for(int i=0;i<numberOfItems;i++){
+            itemList[i] = new Item();
+        }
+    }
 
 }
