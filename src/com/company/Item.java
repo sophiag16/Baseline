47ec4a6aa5a098ed7166b20b8ba322cf1b8ca9a1
sophiag <sophiag@INsophiag.local>
2015-08-24 17:40:09 +0530

    first commit for baseline

---
+++ src/com/company/Item.java
@@ -0,0 +1,25 @@
+package com.company;
+
+/**
+ * Created by sophiag on 24/08/15.
+ */
+public class Item {
+    private String itemName;
+    private int price;
+    private boolean isItImported;
+    private int salesTax, importTax;
+
+    public Item(String itemName, int price, boolean isItImported){
+        this.itemName = itemName;
+        this.price = price;
+        this.isItImported = isItImported;
+
+        setTaxValues();
+    }
+
+    private void setTaxValue(){
+        if(isItImported){
+            
+        }
+    }
+}
---
+++ src/com/company/ShoppingBasket.java
@@ -0,0 +1,9 @@
+package com.company;
+
+/**
+ * Created by sophiag on 24/08/15.
+ */
+public class ShoppingBasket {
+    int numberOfItems;
+
+}
