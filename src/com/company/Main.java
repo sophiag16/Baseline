c21a7a6ebdff59dc42317b10d009b5ef54b24959
sophiag <sophiag@INsophiag.local>
2015-08-24 19:05:47 +0530

    seventh

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,27 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //String[] items = {"book","12.49","music","14.99","food","0.85"}
-
-        //ShoppingBasket myBasket = new ShoppingBasket(items);
-
-        Item[] list = new Item[3];
-
-
-
-        list[0] = new Item("book",12.49,false);
-        list[1] = new Item("music_CD",14.99,false);
-        list[2] = new Item("food",0.85,false);
-
-        int totalPrice = 0;
-        int totalTax = 0;
-
-        for(int i=0;i<3;i++) {
-            System.out.println(list[i].itemName+" "+list[i].price);
-            totalPrice += list[i].price;
-            totalTax += list[i].totalTax;
-        }
-
-        System.out.println("Total price is:"+totalPrice+"\nTotal tax is:"+totalTax);
+        ShoppingBasket myBasket = new ShoppingBasket(3);
+        myBasket.printReceipt();
     }
 }
--- src/com/company/ShoppingBasket.java
+++ src/com/company/ShoppingBasket.java
@@ -4,15 +4,37 @@ package com.company;
  * Created by sophiag on 24/08/15.
  */
 public class ShoppingBasket {
-    int numberOfItems;
-    Item[] itemList;
+    int numberOfItems = 3;
+    Item[] list = new Item[numberOfItems];
+    double totalPrice = 0;
+    double totalTax = 0;
 
     public ShoppingBasket(int numberOfItems){
         this.numberOfItems = numberOfItems;
 
-        /*for(int i=0;i<numberOfItems;i++){
-            itemList[i] = new Item();
-        }*/
+        list[0] = new Item("book",12.49,false);
+        list[1] = new Item("music_CD",14.99,false);
+        list[2] = new Item("food",0.85,false);
+
+        calculateTotal();
+
+     }
+
+    public void calculateTotal(){
+        for(int i=0;i<3;i++) {
+            //System.out.println(list[i].itemName+" "+list[i].price);
+            totalPrice += list[i].price;
+            totalTax += list[i].totalTax;
+        }
+
+        //System.out.println("Total price is:"+totalPrice+"\nTotal tax is:"+totalTax);
+    }
+
+    public void printReceipt(){
+        for(int i=0;i<3;i++) {
+            System.out.println(list[i].itemName+" "+list[i].price);
+        }
+        System.out.println("Total price is:"+totalPrice+"\nTotal tax is:"+totalTax);
     }
 
 }
