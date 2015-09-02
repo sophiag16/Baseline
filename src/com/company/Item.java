12acf599530172f31874fc1e7f2693a4bf10860d
sophiag16 <sophia.g16@gmail.com>
2015-09-02 22:46:49 +0530

    ksdk

--- src/com/company/Item.java
+++ src/com/company/Item.java
@@ -1,63 +1,56 @@
 package com.company;
 
+import java.lang.*;
 /**
  * Created by sophiag on 24/08/15.
  */
 public class Item {
-    String itemName,type;
-    double price;
+    String itemName,type="general";
+    double price=0.0;
     double shelfPrice;
     boolean isItImported;
-    boolean isSalesTaxApplicable;
     double salesTax, importTax, totalTax;
+    String[] exemptSalesTax = {"chocolate","chocolates","book"};
 
-    public Item(String itemName, double price, boolean isItImported, String type){
-        this.itemName = itemName;
-        this.shelfPrice = price;
-        this.isItImported = isItImported;
-        this.type = type;
-
+    public Item(String itemList){
+        String[] splitItemList = itemList.split(" ");
+        int len = splitItemList.length;
+        shelfPrice = Double.parseDouble(splitItemList[len - 1]);
+        isItImported = itemList.contains("imported");
+        for(int i=0;i<3;i++){
+            if(itemList.contains(exemptSalesTax[i])){
+                type = "exempt";
+            }
+        }
 
         setTaxValues();
+
+        itemName = splitItemList[0];
+        for(int i=1; i<splitItemList.length-2; i++) {
+            itemName += " " + splitItemList[i];
+        }
+        itemName += ": "+Double.toString(price);
     }
 
     private void setTaxValues(){
-        if(type=="medicine"||type=="book"||type=="food"){
+        if(type=="exempt"){
             salesTax = 0.0;
-            isSalesTaxApplicable = false;
         }
         else{
-            isSalesTaxApplicable = true;
+            salesTax = 0.1 * shelfPrice;
+            salesTax = Math.round(salesTax / 0.05) * 0.05;
+
         }
 
-        double taxRate;
-        if(isItImported&&isSalesTaxApplicable) {
-            taxRate = 15.0;
-        }
-        else if(isItImported) {
-            taxRate = 5.0;
-        }
-        else if(isSalesTaxApplicable) {
-            taxRate = 10.0;
-        }
-        else {
-            taxRate = 0.0;
+        if(isItImported){
+            importTax = 0.05 * shelfPrice;
+            importTax = Math.round(importTax / 0.05) * 0.05;
         }
 
-        //shelfPrice = (price/((taxRate/100)+1));
-        totalTax = (taxRate/100.0) * shelfPrice;
-
+        totalTax = salesTax + importTax;
         price = shelfPrice + totalTax;
-
-        if(isItImported&&isSalesTaxApplicable){
-            salesTax = totalTax*(2/3);
-            importTax = totalTax*(1/3);
-        }
-        else if(isItImported)
-            importTax = totalTax;
-        else if(isSalesTaxApplicable)
-            salesTax = totalTax;
+        System.out.println(price);
+        price = Math.round(price / 0.01) * 0.01;
+        System.out.println(price);
     }
-
-
 }
--- src/com/company/ShoppingBasket.java
+++ src/com/company/ShoppingBasket.java
@@ -1,37 +1,52 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by sophiag on 24/08/15.
  */
 public class ShoppingBasket {
-    int numberOfItems = 3;
-    Item[] list = new Item[numberOfItems];
+    Item[] list = new Item[10];
     double totalPrice = 0;
     double totalTax = 0;
+    int count = 0;
 
     public ShoppingBasket(){
 
-        list[0] = new Item("book",12.49,false,"book");
-        list[1] = new Item("musicCD",14.99,false,"general");
-        list[2] = new Item("chocolateBar",0.85,false,"food");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String itemList = "";
+        while(true){
+            try {
+                itemList = reader.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if(itemList.equals("done"))
+                break;
+            list[count++] = new Item(itemList);
+        }
 
         calculateTotal();
 
      }
 
     public void calculateTotal(){
-        for(int i=0;i<3;i++) {
+        for(int i=0;i<count;i++) {
             totalPrice += list[i].price;
             totalTax += list[i].totalTax;
         }
 
+        totalPrice = Math.ceil(totalPrice * 100) / 100;
+
     }
 
     public void printReceipt(){
-        for(int i=0;i<3;i++) {
-            System.out.println(list[i].itemName+" "+list[i].price);
+        for(int i=0;i<count;i++) {
+            System.out.println(list[i].itemName);
         }
-        System.out.println("Total price is:"+totalPrice+"\nTotal tax is:"+totalTax);
+        System.out.println("Sales Taxes: "+totalTax+"\nTotal: "+totalPrice);
     }
 
 }
